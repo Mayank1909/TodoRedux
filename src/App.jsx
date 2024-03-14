3f1214a4fa63c7ee0795f70ad299a38c325eb57b
Mayank1909 <78377998+Mayank1909@users.noreply.github.com>
2024-03-14 21:41:10 +0530

    added redux made todo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todo.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Learn about redux toolkit
+        <AddTodo />
+        <Todos />
+      </h1>
+    </>
+  )
+}
+
+export default App
